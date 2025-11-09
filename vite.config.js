d392c303151d35396dff0cca51888e155ffa136c
chen4023 <ads4023@naver.com>
2025-11-09 18:44:18 +0900

    fix: 개발모드용 config 변경

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
-    base: env.VITE_BASE_PATH || "/",
+    base: mode === "development" ? "/" : env.VITE_BASE_PATH || "/",
     build: {
       outDir: "dist",
       assetsDir: "assets",
