ed1865b4396f5d91593180e2494e90c207262a91
im-binary <pongpong0121@kakao.com>
2025-11-09 17:06:04 +0900

    fix: serviceWorker url 수정

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
+import { PageLayout } from "./components/index.js";
+
 const enableMocking = () =>
   import("./mocks/browser.js").then(({ worker }) =>
     worker.start({
+      serviceWorker: {
+        url: "/front_7th_chapter2-1/mockServiceWorker.js",
+      },
       onUnhandledRequest: "bypass",
     }),
   );
 
-function main() {}
+function main() {
+  document.body.innerHTML = PageLayout({ children: "<h1>Hello World</h1>" });
+}
 
 // 애플리케이션 시작
 if (import.meta.env.MODE !== "test") {
