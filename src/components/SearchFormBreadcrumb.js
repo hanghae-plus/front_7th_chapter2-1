4de007830354254363dfd5966ea0877582515053
JaeHyunGround <gothddlek@naver.com>
2025-11-10 16:34:34 +0900

    feat : SearchForm 영역의 브레드크럼 컴포넌트 분리
    - SearchForm 영역의 브레드크럼 부분을 컴포넌트로 분리했습니다.
    - 쿼리 스트링에 따라 브레드크럼 출력을 제어했습니다.

---
+++ src/components/SearchFormBreadcrumb.js
@@ -0,0 +1,24 @@
+const category1Breadcrumb = ({ category1 }) => {
+  return /* html */ `
+    <span class="text-xs text-gray-500">&gt;</span>
+    <button data-breadcrumb="category1" data-category1=${category1} class="text-xs hover:text-blue-800 hover:underline">${category1}</button>
+    `;
+};
+
+const category2Breadcrumb = ({ category2 }) => {
+  return /* html */ `
+      <span class="text-xs text-gray-500">&gt;</span>
+      <span class="text-xs text-gray-600 cursor-default">${category2}</span>
+    `;
+};
+
+export const SearchFormBreadcrumb = ({ params }) => {
+  const category1 = params.get("category1");
+  const category2 = params.get("category2");
+
+  return /* html */ `
+    <button data-breadcrumb="reset" class="text-xs hover:text-blue-800 hover:underline">전체</button>
+    ${category1 ? category1Breadcrumb({ category1 }) : ""}
+    ${category1 && category2 ? category2Breadcrumb({ category2 }) : ""}
+    `;
+};
