5b01450067073937ed024c1008cb9464e8c3e4e4
rlacodud <rlacodud0313@naver.com>
2025-11-10 23:24:19 +0900

    build: 배포 설정

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/front_7th_chapter2-1/" : "/",
+
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+
   test: {
     globals: true,
     environment: "jsdom",
