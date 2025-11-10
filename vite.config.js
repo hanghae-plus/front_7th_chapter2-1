2c1d988be19f2556e4dd4f1e04901b4cf56172cd
jy0813 <busbug3@gmail.com>
2025-11-10 15:41:02 +0900

    chore: prettier 및 절대경로 설정

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
-import { defineConfig } from "vitest/config";
+import { fileURLToPath, URL } from 'url';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   test: {
     globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/setupTests.js",
-    exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
+    exclude: ['**/e2e/**', '**/*.e2e.spec.js', '**/node_modules/**'],
     poolOptions: {
       threads: {
         singleThread: true,
