b94cadc30221c44c7280ffd50861467ae8e98772
윤지 <yoonhihi97@gmail.com>
2025-11-10 16:15:43 +0900

    feat: 헤더 타입별 분리 (기본/상세)

--- src/pages/PageLayout.js
+++ src/pages/PageLayout.js
@@ -1,9 +1,16 @@
 import { Header, Footer } from "../components/common/index.js";
 
-export const PageLayout = ({ children }) => {
+/**
+ * 페이지 레이아웃
+ * @param {Object} props
+ * @param {string} props.children - 페이지 컨텐츠
+ * @param {string} props.headerType - 헤더 타입 ("default" | "detail")
+ * @param {string} props.headerTitle - 헤더 타이틀
+ */
+export const PageLayout = ({ children, headerType = "default", headerTitle = "쇼핑몰" }) => {
   return `
     <div class="min-h-screen bg-gray-50">
-      ${Header()}
+      ${Header({ type: headerType, title: headerTitle })}
       <main class="max-w-md mx-auto px-4 py-4">${children}</main>
       ${Footer()}
     </div>
