48759a78ac873da119018a6e8f525f38f2938d8c
Leehyunji0715 <hjlee@bizflow.com>
2025-11-10 16:23:24 +0900

    feat: load product first then related products in detail page

--- src/main.js
+++ src/main.js
@@ -20,12 +20,13 @@ const render = async () => {
     $root.innerHTML = DetailPage({ loading: true });
     const productId = location.pathname.split("/")[2];
     const product = await getProduct(productId);
-    const relatedProducts = !product.error
-      ? (await getProducts({ page: 1, category2: product.category2 })).products.filter(
-          (product) => product.productId !== productId,
-        )
-      : [];
-    $root.innerHTML = DetailPage({ loading: false, product: product.error ? undefined : product, relatedProducts });
+    $root.innerHTML = DetailPage({ loading: false, product: product.error ? undefined : product, relatedProducts: [] });
+    if (!product.error) {
+      const relatedProducts = (await getProducts({ page: 1, category2: product.category2 })).products.filter(
+        (product) => product.productId !== productId,
+      );
+      $root.innerHTML = DetailPage({ loading: false, product: product, relatedProducts });
+    }
   } else {
     $root.innerHTML = NotFoundPage();
   }
