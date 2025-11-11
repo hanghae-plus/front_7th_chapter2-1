d23c5f0712ee09c4e9082f35f43eadb77cd94ceb
rlacodud <rlacodud0313@naver.com>
2025-11-12 02:42:00 +0900

    feat: CategoryFilter component 추가

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,15 +1,41 @@
+import { store } from "../../store";
+
 export const CategoryFilter = () => {
+  const { isLoaded, categories } = store.state;
+
   return `
   <div class="space-y-2">
     <div class="flex items-center gap-2">
       <label class="text-sm text-gray-600">카테고리:</label>
       <button data-breadcrumb="reset" class="text-xs hover:text-blue-800 hover:underline">전체</button>
     </div>
-    <!-- 1depth 카테고리 -->
-    <div class="flex flex-wrap gap-2">
-      <div class="text-sm text-gray-500 italic">카테고리 로딩 중...</div>
-    </div>
-    <!-- 2depth 카테고리 -->
+    ${
+      isLoaded
+        ? `
+        <!-- 1depth 카테고리 -->
+        <div class="flex flex-wrap gap-2">
+          ${Object.keys(categories)
+            .map(
+              (category) =>
+                `
+          <button data-category1="${category}" class="category1-filter-btn text-left px-3 py-2 text-sm rounded-md border transition-colors
+              bg-white border-gray-300 text-gray-700 hover:bg-gray-50">
+            ${category}
+          </button>
+          `,
+            )
+            .join("")}
+        </div>
+        <!-- 2depth 카테고리 -->
+        `
+        : `
+      <!-- 1depth 카테고리 -->
+      <div class="flex flex-wrap gap-2">
+        <div class="text-sm text-gray-500 italic">카테고리 로딩 중...</div>
+      </div>
+      <!-- 2depth 카테고리 -->
+      `
+    }
   </div>
   `;
 };
