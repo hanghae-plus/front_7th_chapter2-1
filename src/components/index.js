f252b7f8250a540aa6398286380da74d9b1807ec
김민석 <kms951018@douzone.com>
2025-11-11 14:03:24 +0900

    feat: 검색 및 필터 부분 컴포넌트화 작업

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
 export * from "./Header";
 export * from "./Footer";
-export * from "./ProductList";
-export * from "./SearchForm";
+export * from "./product/ProductList";
+export * from "./search/SearchForm";
