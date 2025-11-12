20298eaae332562136373b42c64d8a80740c6b77
Yein Lee <zxth47e@gmail.com>
2025-11-12 20:55:53 +0900

    chore: tsconfig 수정 및 alias 설정

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   base: "/front_7th_chapter2-1/",
   build: {
     outDir: "dist",
   },
+  plugins: [tsconfigPaths()],
 });
