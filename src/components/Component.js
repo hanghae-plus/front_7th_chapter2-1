ca5ccae0805228ce4301284bf227519b6c5ebcc9
Leehyunji0715 <hjlee@bizflow.com>
2025-11-12 15:34:06 +0900

    feat: add Component class as interface

---
+++ src/components/Component.js
@@ -0,0 +1,34 @@
+export class Component {
+  constructor($container, props = {}) {
+    this.$container = $container;
+    this.props = props;
+    this.state = {};
+    this.mount();
+    this.render();
+  }
+
+  mount() {}
+
+  unmount() {
+    this.$container.innerHTML = "";
+  }
+
+  setState(newState) {
+    this.state = { ...this.state, ...newState };
+    this.render();
+  }
+
+  template() {
+    return "";
+  }
+
+  render() {
+    this.$target.innerHTML = this.template();
+  }
+
+  async updateProps(newProps) {
+    this.props = { ...this.props, ...newProps };
+    this.render();
+    return Promise.resolve();
+  }
+}
