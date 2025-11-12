0805f2a876e49c127071f7df7255d638b430cafd
rlacodud <rlacodud0313@naver.com>
2025-11-13 02:51:10 +0900

    feat: Layout component Toast, CartModal 추가

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,12 +1,17 @@
-import { Footer, Header } from "../components/index.js";
+import { store } from "../../store.js";
+import { CartModal, Footer, Header, Toast } from "../components/index.js";
 
 export const Layout = ({ children }) => {
+  const toasState = store.state.toastState;
+
   return `
     <div class="min-h-screen bg-gray-50">
       ${Header()}
       <main class="max-w-md mx-auto px-4 py-4">
         ${children}
       </main>
+      ${toasState && Toast()}
+      ${CartModal()}
       ${Footer()}
     </div>
   `;
