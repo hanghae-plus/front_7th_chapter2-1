e567105910690d6d78a55231472d689f1b5b29e6
hanseul jung <hanseul56524@gmail.com>
2025-11-13 22:37:46 +0900

    chore: github pages 배포 관련 설정

--- vite.config.js
+++ vite.config.js
@@ -12,4 +12,8 @@ export default defineConfig({
       },
     },
   },
+  base: "/front_7th_chapter2-1/", // Repository 이름과 일치
+  build: {
+    outDir: "dist", // vite 기본 값이 `dist` 이므로 생략 가능
+  },
 });
