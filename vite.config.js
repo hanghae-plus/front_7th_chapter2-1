9b4a98ca29c1421d05a704d4ff7563207bdc45d5
rlacodud <rlacodud0313@naver.com>
2025-11-14 00:20:20 +0900

    build: 빌드 설정 수정

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
+import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig(({ mode }) => ({
   base: mode === "production" ? "/front_7th_chapter2-1/" : "/",
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 }));
