5781f46b8766abbfaf763a890d95f874131da73e
이정민 <ljm5138@douzone.com>
2025-11-13 10:07:58 +0900

    feat: PageLayout 플러그인 추가

--- src/pages/PageLayout.js
+++ src/pages/PageLayout.js
@@ -1,7 +1,7 @@
 import { Header, Footer } from "../components";
 
 export const PageLayout = ({ children }) => {
-  return `
+  return /* html */ `
         <div class="min-h-screen bg-gray-50">
             ${Header()}
             <main class="max-w-md mx-auto px-4 py-4">
