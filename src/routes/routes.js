adf2e1b09cf35b9b1838bb54d8159f09c259c77b
chacha <86880916+hansejun@users.noreply.github.com>
2025-11-13 16:43:05 +0900

    refactor: routes 레이아웃 수정

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,11 @@
 import { NotFoundPage } from "../pages/not-found/NotFoundPage";
 import { HomePage } from "../pages/home/HomePage";
 import { ProductDetailPage } from "../pages/product-detail/ProductDetailPage";
+import { MainLayout } from "../components/layout/MainLayout";
+import { DetailLayout } from "../components/layout/DetailLayout";
 
 export const routes = [
-  { path: "/", component: HomePage },
-  { path: "/product/:productId", component: ProductDetailPage },
-  { path: "/*", component: NotFoundPage },
+  { path: "/", component: HomePage, layout: MainLayout },
+  { path: "/product/:productId", component: ProductDetailPage, layout: DetailLayout },
+  { path: "/*", component: NotFoundPage, layout: MainLayout },
 ];
