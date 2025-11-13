b0e8713ff30364e41542bd25bbbed4bc7e90e436
Yongtae Park <samuel.park@mz.co.kr>
2025-11-14 03:10:58 +0900

    refactor: refactor initial render logic for routes

--- src/main.js
+++ src/main.js
@@ -48,23 +48,21 @@ async function main() {
   const appState = appStore.getState();
 
   /* Initial Render */
-  if (relativePath === homeRoute.path) {
-    $root.innerHTML = `
-      ${homeRoute.render({ loading: true, cart: appState.cart })}
-    `;
-    const props = await homeRoute.loader();
-    $root.innerHTML = `
-      ${homeRoute.render(props)}
-    `;
-  } else if (productDetailRoute.pattern.test(relativePath)) {
+  // if (relativePath === homeRoute.path) {
+  //   $root.innerHTML = `
+  //     ${homeRoute.render({ loading: true, cart: appState.cart })}
+  //   `;
+  //   const props = await homeRoute.loader();
+  //   $root.innerHTML = `
+  //     ${homeRoute.render(props)}
+  //   `;
+  // } else
+
+  if (productDetailRoute.pattern.test(relativePath)) {
     const id = relativePath.split("/")[2];
-    $root.innerHTML = `
-      ${productDetailRoute.render({ loading: true, cart: appState.cart })}
-    `;
+    $root.replaceChildren(productDetailRoute.render({ loading: true, cart: appState.cart }));
     const props = await productDetailRoute.loader({ id });
-    $root.innerHTML = `
-      ${productDetailRoute.render(props)}
-    `;
+    $root.replaceChildren(productDetailRoute.render(props));
   } else {
     console.log("[Initial Render] Not Found", relativePath);
     history.pushState(null, "", `${basePath}404`);
@@ -205,23 +203,11 @@ async function main() {
         ${homeRoute.render(props)}
       `;
       appStore.setListLoading(false);
-    } else if (event.target.closest("#add-to-cart-btn")) {
-      console.log("[Click Event] add-to-cart-btn", event);
-      const productId = event.target.dataset.productId;
-      if (!productId) return;
-      appStore.addToCart(productId, appState.cartItemCount);
-      showToastMessage(TOAST_MESSAGE_MAP.ADD_TO_CART, "success");
     } else if (event.target.closest("#cart-icon-btn")) {
       console.log("[Click Event] cart-icon-btn", event);
       $cartModalRoot.innerHTML = `
         ${CartModal({ cart: appState.cart, selectedCartIds: appState.selectedCartIds })}
       `;
-    } else if (event.target.id === "quantity-decrease") {
-      console.log("[Click Event] quantity-decrease", event);
-      appStore.subtractCartItemCount();
-    } else if (event.target.id === "quantity-increase") {
-      console.log("[Click Event] quantity-increase", event);
-      appStore.addCartItemCount();
     } else if (event.target.closest("[data-link]")) {
       console.log("[Click Event] link", event);
 
