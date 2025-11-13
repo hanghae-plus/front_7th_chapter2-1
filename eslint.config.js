b7338dc13e7f79f4e69b07b8bfeaf6f3b8facbdd
devmineee <devmineee@gmail.com>
2025-11-14 00:55:15 +0900

    fix: yml 파일 수정

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,9 @@ import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  {
+    ignores: ["dist/**", "node_modules/**", "playwright-report/**", "test-results/**", "*.config.js", "e2e/**"],
+  },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   eslintPluginPrettier,
